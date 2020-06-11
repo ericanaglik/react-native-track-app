00feb1f69348d4c0699a063dde5975b32c85c1f1
erica2605@live.com <erica2605@live.com>
2020-06-11 13:57:42 -0400

    🌟 [updated] navigation

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Button } from 'react-native'
 
 const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+  return <>
+  <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+  <Button
+    title="Go to Track Detail"
+    onPress={() => NavigationPreloadManager.navigate('TrackDetail')}
+  />
+  </>
 }
 
 const styles = StyleSheet.create({})
