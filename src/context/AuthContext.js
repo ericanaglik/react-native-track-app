947a49ebea1750d24eb8d9b054821ee87fac948e
erica2605@live.com <erica2605@live.com>
2020-06-15 20:35:23 -0400

    🌟 [updated] login functionality with context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,7 +10,9 @@ const authReducer = (state, action) => {
     case 'signin':
       return { errorMessage: '', token: action.payload }
     case 'clear_error_message':
-    return { ...state, errorMessage: '' }
+      return { ...state, errorMessage: '' }
+    case 'signout':
+      return { token: null, errorMessage: ''}
     default:
       return state
   }
@@ -59,11 +61,12 @@ const signin = (dispatch) => async ({ email, password }) => {
 }
 
 
-const signout = (dispatch) => {
-  return () => {
-
-  }
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token')
+  dispatch({ type: 'signout' })
+  navigate('loginFlow')
 }
+
 
 export const { Provider, Context } = createDataContext(
   authReducer,
