d7f83f4ac0a61a0a8818cf064a656231b4395fc6
erica2605@live.com <erica2605@live.com>
2020-06-16 16:07:37 -0400

    🌟 [updated] location

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = increment => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees
+      longitude: -76.82660292525318 + increment * tenMetersWithDegrees,
+      latitude: 39.29922064329101 + increment * tenMetersWithDegrees
     }
   };
 };
