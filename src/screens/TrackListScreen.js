994dd8811a14e7ec1551079b239e234f7213a234
erica2605@live.com <erica2605@live.com>
2020-06-22 15:30:48 -0400

    🌟 [updated] to show list of created tracks

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useContext } from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import { NavigationEvents } from 'react-navigation'
+import { Context as TrackContext } from '../context/TrackContext'
 
 const TrackListScreen = () => {
-  return <>
-  <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-  <Button
-    title="Go to Track Detail"
-    onPress={() => NavigationPreloadManager.navigate('TrackDetail')}
-  />
-  </>
+  const { state, fetchTracks } = useContext(TrackContext)
+  return (
+    <>
+      <NavigationEvents onWillFocus={fetchTracks}/>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return <TouchableOpacity>
+            <ListItem chevron title={item.name}/>
+          </TouchableOpacity>
+        }}
+      />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({})
