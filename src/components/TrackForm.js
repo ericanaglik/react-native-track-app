cdeecdc1a4a68c55eb766444fbc9a996376c0143
erica2605@live.com <erica2605@live.com>
2020-06-22 14:23:34 -0400

    🌟 [updated] to save recordings

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -14,10 +14,18 @@ const TrackForm = () => {
           placeholder="Enter name"
         />
       </Spacer>
+      <Spacer>
       { recording
         ? <Button title="Stop" onPress={stopRecording}/> 
         : <Button title="Start Recording" onPress={startRecording}/> 
       }
+      </Spacer>
+      <Spacer>
+      { !recording && locations.length 
+        ? <Button title="Save Recording" />
+        : null
+      }
+      </Spacer>
     </>
   )
 }
